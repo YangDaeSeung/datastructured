25fd514417f64e710fa007fb32bd7f0b4097c8e0
YangDaeSeung <daeseung0216@gmail.com>
2018-04-12 17:39:08 +0900

    uniy c#

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,6 @@ namespace NCS.CShap
     class Item
     {
         public enum eItemKind { Weapon, Armor, Acc, Etc }
-
         public Item(eItemKind eItemkind, string name, string comment, Status sStatus, int nGold)
         {
             m_eItemKind = eItemkind;
@@ -55,33 +54,63 @@ namespace NCS.CShap
     //c#에서 구조체는 정적할당된다.
     struct Status  //플레이어의 증가능력치를 구조체로 만들어 사용한다.
     {
-        public int m_nHP, m_nMaxHP;
-        public int m_nMP, m_nMaxMP;
+        public int m_nHP;
+        public int m_nMP;
         public int m_nStr;
         public int m_nDef;
         public int m_nInt;
 
-        public Status(int nStr = 0, int nDef = 0, int nInt = 0, int nHP = 0, int nMaxHP = 0, int nMP = 0, int nMaxMP = 0)
+        public Status(int nStr = 0, int nDef = 0, int nInt = 0, int nHP = 0, int nMP = 0)
         {
             m_nHP = nHP;
-            m_nMaxHP = nMaxHP;
             m_nMP = nMP;
-            m_nMaxMP = nMaxMP;
             m_nStr = nStr;
             m_nDef = nDef;
             m_nInt = nInt;
         }
+        public void AddStatus(int var)
+        {
+            m_nHP += var;
+            m_nMP += var;
+            m_nStr += var;
+            m_nInt += var;
+            m_nDef += var;
+        }
         public static Status operator +(Status a, Status b)
         {
-            return new Status(a.m_nHP + b.m_nHP, a.m_nMaxHP + b.m_nMaxHP,
-                                    a.m_nMP + b.m_nMP, a.m_nMaxMP + b.m_nMaxMP,
+            return new Status(a.m_nHP + b.m_nHP, a.m_nMP + b.m_nMP,
                                     a.m_nStr + b.m_nStr, a.m_nDef + b.m_nDef, a.m_nInt + b.m_nInt);
         }
         public static Status operator -(Status a, Status b)
         {
-            return new Status(a.m_nHP - b.m_nHP, a.m_nMaxHP - b.m_nMaxHP,
-                                    a.m_nMP - b.m_nMP, a.m_nMaxMP - b.m_nMaxMP,
+            return new Status(a.m_nHP - b.m_nHP, a.m_nMP - b.m_nMP,
                                     a.m_nStr - b.m_nStr, a.m_nDef - b.m_nDef, a.m_nInt - b.m_nInt);
+        }
+    }
+
+    class ItemManager //아이템매니저: 아이템을 보관해놓고 가져다쓰는 물류센터
+    {
+        public enum eItem { WoodSword, BoneSword, WoodArmor, BoneArmor, WoodRing, BoneRing, HPPotion, MPPotion, Stone, Boom, MAX };
+        List<Item> m_listItemList;
+
+        public ItemManager()
+        {
+            m_listItemList = new List<Item>((int)eItem.MAX);
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "목검", "데미지증가", new Status(100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "본소드", "데미지증가", new Status(200), 200));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "나무갑옷", "방어력증가", new Status(0, 100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "본아머", "방어력증가", new Status(0, 200), 200));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "나무반지", "체력증가", new Status(0, 0, 0, 100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "해골반지", "체력증가", new Status(0, 0, 0, 200), 200));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "힐링포션", "HP회복", new Status(0, 0, 0, 100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "마나포션", "MP회복", new Status(0, 0, 0, 0, 100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "짱돌", "단일개체데미지", new Status(100), 100));
+            m_listItemList.Add(new Item(Item.eItemKind.Weapon, "폭탄", "다수개체데미지", new Status(200), 200));
+        }
+
+        public Item GetItem(eItem idx)
+        {
+            return m_listItemList[(int)idx];
         }
     }
 
@@ -89,6 +118,9 @@ namespace NCS.CShap
     {
         //스텟
         Status m_cStatus;
+
+        public int m_nMaxHP;
+        public int m_nMaxMP;
         //이름
         string m_strName;
         int m_nLv;
@@ -97,20 +129,26 @@ namespace NCS.CShap
         int m_nGold; //소지금
 
         List<Item> m_listIventory = new List<Item>(); //인벤토리.
-        List<Item> m_listEqument = new List<Item>(); //장비함.
-        public enum eEqumentKind { Weapon, Armor, Acc }
+        List<Item> m_listEqument = new List<Item>((int)eEqumentKind.MAX); //장비함.
+        public enum eEqumentKind { Weapon, Armor, Acc, MAX }
 
-        public Player(string name, int nHP, int nMP, int nStr, int nInt, int nDef)
+        public Player(string name, int nHP = 100, int nMP = 100, int nStr = 10, int nInt = 10, int nDef = 10, int nExp = 10, int nGold = 0)
         {
-            m_cStatus = new Status(nStr, nDef, nInt, nHP, nHP, nMP, nMP);
+            m_cStatus = new Status(nStr, nDef, nInt, nHP, nMP);
+            m_nMaxHP = nHP;
+            m_nMaxMP = nMP;
             m_strName = name;
+            m_nExp = nExp;
+            m_nGold = nGold;
+            m_nLv = 1;
+            m_listEqument = new List<Item>((int)eEqumentKind.MAX); //장비함.
+            for (int i = 0; i < (int)eEqumentKind.MAX; i++)
+                m_listEqument.Add(null);
         }
-
         public void Attack(Player cTarget)
         {
-            cTarget.m_cStatus.m_nHP = cTarget.m_cStatus.m_nHP - m_cStatus.m_nStr;
+            cTarget.m_cStatus.m_nHP = cTarget.m_cStatus.m_nHP - (m_cStatus.m_nStr - cTarget.m_cStatus.m_nDef);
         }
-
         public bool Dead()
         {
             if (m_cStatus.m_nHP <= 0)
@@ -119,32 +157,62 @@ namespace NCS.CShap
             }
             return false;
         }
-
         public void StillItem(Player cTarget)
         {
             SetInvetory(cTarget.GetInvetory(0));
             m_nExp += cTarget.m_nExp;
         }
-
         public void Recovery()
         {
-            m_cStatus.m_nHP = m_cStatus.m_nMaxHP;
-            m_cStatus.m_nMP = m_cStatus.m_nMaxMP;
+            m_cStatus.m_nHP = m_nMaxHP;
+            m_cStatus.m_nMP = m_nMaxMP;
+        }
+
+        public bool LvUp(int var = 10, int maxexp = 100)
+        {
+            if (m_nExp > maxexp)
+            {
+                m_cStatus.AddStatus(var);
+                m_nExp -= maxexp;
+                m_nMaxHP += var;
+                m_nMaxMP += var;
+                return true;
+            }
+
+            return false;
         }
 
         public void SetInvetory(Item item)
         {
             m_listIventory.Add(item);
         }
-
         public Item GetInvetory(int idx)
         {
             return m_listIventory[idx];
         }
-
         public void DeleteInventory(Item item)
         {
             m_listIventory.Remove(item);
+        }
+
+        public bool Buy(int nIventoryIdx, Player cTarget)
+        {
+            Item cItem = cTarget.GetInvetory(nIventoryIdx);
+
+            if (cItem.Gold <= m_nGold)
+            {
+                SetInvetory(cItem);
+                m_nGold -= cItem.Gold;
+                return true;
+            }
+
+            return false;
+        }
+        public void Sell(int nIventoryIdx)
+        {
+            Item cItem = GetInvetory(nIventoryIdx);
+            DeleteInventory(cItem);
+            m_nGold += cItem.Gold;
         }
 
         public void SetEquemnt(Item item) //아이템장착
@@ -174,20 +242,22 @@ namespace NCS.CShap
         public void Show()
         {
             Console.WriteLine(String.Format("######## {0} ########", m_strName));
-            Console.WriteLine(String.Format("HP:{0}/{1}", m_cStatus.m_nHP, m_cStatus.m_nMaxHP));
-            Console.WriteLine(String.Format("MP:{0}/{1}", m_cStatus.m_nMP, m_cStatus.m_nMaxMP));
+            Console.WriteLine(String.Format("HP:{0}/{1}", m_cStatus.m_nHP, m_nMaxHP));
+            Console.WriteLine(String.Format("MP:{0}/{1}", m_cStatus.m_nMP, m_nMaxMP));
             Console.WriteLine(String.Format("Str/Int/Def:{0}/{1}/{2}", m_cStatus.m_nStr, m_cStatus.m_nInt, m_cStatus.m_nDef));
             Console.WriteLine(String.Format("Lv:{0}", m_nLv));
             Console.WriteLine(String.Format("Exp:{0}", m_nExp));
             Console.WriteLine(String.Format("Gold:{0}", m_nGold));
         }
-
         public void ShowInevtory()
         {
             Console.WriteLine(String.Format("######## Equment ########", m_strName));
             for (int i = 0; i < m_listEqument.Count; i++)
             {
-                Console.WriteLine(String.Format("{0}:{1}", (eEqumentKind)i, m_listEqument[i]));
+                if (m_listEqument[i] != null)
+                    Console.WriteLine(String.Format("{0}:{1}", (eEqumentKind)i, m_listEqument[i].Name));
+                else
+                    Console.WriteLine(String.Format("{0}:{1}", (eEqumentKind)i, "null"));
             }
             Console.WriteLine(String.Format("######## Invetory ########", m_strName));
             for (int i = 0; i < m_listIventory.Count; i++)
@@ -199,38 +269,190 @@ namespace NCS.CShap
 
     class Program
     {
+        enum eStage
+        {
+            CREATE,
+            TOWN,
+            INVENTORY,
+            STORE,
+            FILED, //필드선택
+            BATTLE, //전투지역
+            GAMEOVER,
+            THEEND
+        }
+        enum eMonster { SLIME, SKELTON, BOSS, MAX };
+
+        static eStage m_eStage = eStage.CREATE;
+
+
+        static bool Battle(Player cPlayer, Player cMonster)
+        {
+            Console.WriteLine("####################");
+            if (!cPlayer.Dead())
+                cPlayer.Attack(cMonster);
+            else
+            {
+                m_eStage = eStage.GAMEOVER;
+                return true;
+            }
+            cMonster.Show();
+            if (!cMonster.Dead())
+                cMonster.Attack(cPlayer);
+            else
+            {
+                Console.WriteLine(String.Format("몬스터가 쓰러뜨렸습니다!"));
+                cPlayer.StillItem(cMonster);
+                cPlayer.ShowInevtory();
+                if (cPlayer.LvUp())
+                {
+                    Console.WriteLine("LvUp!");
+                    cPlayer.Show();
+                    m_eStage = eStage.TOWN;
+                    return true;
+                }
+            }
+            cPlayer.Show();
+            Console.WriteLine("###################################");
+            return false;
+        }
+       static void SwapStatus(Status a,Status b)
+        {
+            Status temp = a;
+            a = b;
+            b = temp;
+        }
         static void Main(string[] args)
         {
-            Player cPlayer = new Player("Player", 100, 100, 10, 10, 10);
-            Player cMonster = new Player("Slime", 100, 100, 10, 0, 0);
 
-            Status sStatus = new Status(100);
-            Item cWeapon = new Item(Item.eItemKind.Weapon, "목검", "데미지증가", sStatus, 100);
+            Status sA, sB;
+            sA = new Status(100);
+            sB = new Status(0, 100);
 
-            cMonster.SetInvetory(cWeapon);
+            Console.WriteLine(String.Format("A:{0},{1]", sA.m_nStr, sA.m_nDef));
+            Console.WriteLine(String.Format("B:{0},{1]", sB.m_nStr, sB.m_nDef));
+            SwapStatus(sA, sB);
+            Console.WriteLine(String.Format("{0},{1]", sA.m_nStr, sA.m_nDef));
+            Console.WriteLine(String.Format("{0},{1]", sB.m_nStr, sB.m_nDef));
 
-            while (true)
+            ItemManager cItemManager = new ItemManager();
+            Player cPlayer = null;
+            Player cMonster = null;
+            Player cStore = new Player("Store");
+
+
+            
+
+            cStore.SetInvetory(cItemManager.GetItem(ItemManager.eItem.HPPotion));
+            cStore.SetInvetory(cItemManager.GetItem(ItemManager.eItem.MPPotion));
+            cStore.SetInvetory(cItemManager.GetItem(ItemManager.eItem.Stone));
+            cStore.SetInvetory(cItemManager.GetItem(ItemManager.eItem.Boom));
+
+            eMonster nMonsterIdx = eMonster.SLIME;
+            int nSelect;
+            while (m_eStage != eStage.GAMEOVER)
             {
-                Console.WriteLine("###################################");
-                if (!cPlayer.Dead())
-                    cPlayer.Attack(cMonster);
-                else
+                switch (m_eStage)
                 {
-                    Console.WriteLine("GameOver");
-                    break;
+                    case eStage.CREATE:
+                        Console.WriteLine("케릭터이름을 입력하세요:");
+                        string strName = Console.ReadLine();
+                        cPlayer = new Player(strName, 100, 100, 10, 10, 10, 0, 9999999);
+                        m_eStage = eStage.TOWN;
+                        break;
+                    case eStage.INVENTORY:
+                        cPlayer.ShowInevtory();
+                        Console.WriteLine("어떻게 하시겠습니까? 1:장착, 2:해재 etc:마을");
+                        nSelect = int.Parse(Console.ReadLine());
+                        if (nSelect == 1)
+                        {
+                            nSelect = int.Parse(Console.ReadLine());
+                            Item cItem = cPlayer.GetInvetory(nSelect);
+                            cPlayer.SetEquemnt(cItem);
+                        }
+                        else if (nSelect == 2)
+                        {
+                            nSelect = int.Parse(Console.ReadLine());
+                            Item cItem = cPlayer.GetInvetory(nSelect);
+                            for (int i = 0; i < (int)Player.eEqumentKind.MAX; i++)
+                                Console.Write(String.Format("{0}:{1},", i, (Player.eEqumentKind)i));
+                            cPlayer.ReleaseEquemnt((Player.eEqumentKind)nSelect);
+                        }
+                        else
+                        {
+                            m_eStage = eStage.TOWN;
+                        }
+                        break;
+                    case eStage.STORE:
+                        Console.WriteLine("어떻게 하시겠습니까? 1:구매, 2:판매 etc:마을");
+                        nSelect = int.Parse(Console.ReadLine());
+                        if (nSelect == 1)
+                        {
+                            cStore.ShowInevtory();
+                            nSelect = int.Parse(Console.ReadLine());
+                            cPlayer.Buy(nSelect, cStore);
+                        }
+                        else if (nSelect == 2)
+                        {
+                            cPlayer.ShowInevtory();
+                            nSelect = int.Parse(Console.ReadLine());
+                            cPlayer.Sell(nSelect);
+                        }
+                        else
+                        {
+                            m_eStage = eStage.TOWN;
+                        }
+                        break;
+                    case eStage.TOWN:
+                        cPlayer.Recovery();
+                        cPlayer.ShowInevtory();
+                        Console.Write("마을 입니다. 어디로 가시겠습니까? ");
+                        for (int i = (int)eStage.TOWN + 1; i < (int)eStage.GAMEOVER; i++)
+                            Console.Write(String.Format("{0}:{1},", i, (eStage)i));
+                        m_eStage = (eStage)int.Parse(Console.ReadLine());
+                        break;
+                    case eStage.FILED:
+                        Console.Write("필드을 선택하세요! ");
+                        for (int i = (int)eMonster.SLIME; i < (int)eMonster.MAX; i++)
+                            Console.Write(String.Format("{0}:{1},", i, (eMonster)i));
+                        nMonsterIdx = (eMonster)int.Parse(Console.ReadLine());
+
+                        switch (nMonsterIdx)
+                        {
+                            case eMonster.SLIME:
+                                cMonster = new Player("Slime", 100, 100, 10, 0, 0, 100);
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.WoodSword));
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.WoodArmor));
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.WoodRing));
+                                break;
+                            case eMonster.SKELTON:
+                                cMonster = new Player("Skelton", 200, 200, 20, 10, 0, 100);
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.BoneSword));
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.BoneArmor));
+                                cMonster.SetInvetory(cItemManager.GetItem(ItemManager.eItem.BoneRing));
+                                break;
+                            case eMonster.BOSS:
+                                cMonster = new Player("Boss", 300, 300, 30, 15, 0, 100);
+                                break;
+                            default:
+                                m_eStage = eStage.FILED;
+                                break;
+
+                        }
+
+                        m_eStage = eStage.BATTLE;
+                        break;
+                    case eStage.BATTLE:
+                        Battle(cPlayer, cMonster);
+                        break;
+                    case eStage.GAMEOVER:
+                        Console.WriteLine("GAMEOVER");
+                        break;
+                    case eStage.THEEND:
+                        Console.WriteLine("THE END");
+                        break;
+                    default:
+                        break;
                 }
-                cMonster.Show();
-                if (!cMonster.Dead())
-                    cMonster.Attack(cPlayer);
-                else
-                {
-                    Console.WriteLine(String.Format("몬스터가 쓰러뜨렸습니다!"));
-                    cPlayer.StillItem(cMonster);
-                    cPlayer.ShowInevtory();
-                    break;
-                }
-                cPlayer.Show();
-                Console.WriteLine("###################################");
             }
         }
     }
---
+++ unity C#/cameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+    public float MouseSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 MouseLmit;
+
+    public float scrollSpeed = 2f;
+    public float MinY = 20f;
+    public float MaxY = 60f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+       if(Input.GetKey("w")|| Input.mousePosition.y >= Screen.height-panBorderThickness)
+        {
+            pos.z += MouseSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= Screen.height - panBorderThickness)
+        {
+            pos.z -= MouseSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width- panBorderThickness)
+        {
+            pos.x += MouseSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= Screen.width - panBorderThickness)
+        {
+            pos.x -= MouseSpeed * Time.deltaTime;
+        }
+        float Scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= Scroll * scrollSpeed *100f* Time.deltaTime; 
+        pos.x = Mathf.Clamp(pos.x, -MouseLmit.x, MouseLmit.x);
+        pos.y = Mathf.Clamp(pos.y, MinY, MaxY);
+        pos.z = Mathf.Clamp(pos.z, -MouseLmit.y,MouseLmit.y);
+        transform.position = pos;
+    }
+}
